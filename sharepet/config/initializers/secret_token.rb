12102054c8fb003975b5a87faf80ab4c0ac3f357
caiquepereira@gmail.com <saasbook@saasbook.(none)>
2015-11-11 06:58:40 -0400

    Created scaffold of the user

---
+++ sharepet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharepet::Application.config.secret_token = '1e4fbc0ebe4aa74df30ecf79a2435b6debee53360d125df7323295b003d4154ac1386eeea6c37996aeb71d38136380c9be4634053699adf1869662fc8ef3893e'
---
+++ sharepet/db/migrate/20151107202311_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :username
+      t.string :password
+      t.string :address
+      t.string :phone
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
